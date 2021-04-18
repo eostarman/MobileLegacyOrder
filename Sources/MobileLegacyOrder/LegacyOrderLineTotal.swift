d406a12ae5b7974731e5e3fd6ca67e53076b2ad3
Michael Rutherford <doomiigo@gmail.com>
2021-04-18 01:23:59 -0400

    Redid invoice printing to work from the new Order format (not from the LegacyOrder) - actually, it works against the new Invoice struct Put in a single-screen delivery view for a customer (as a POC) - it lists the order numbers, and when you tap on one I show where it is on the truck or the PDF invoice view

--- Sources/MobileLegacyOrder/LegacyOrderLineTotal.swift
+++ Sources/MobileLegacyOrder/LegacyOrderLineTotal.swift
@@ -1,42 +1,42 @@
+////
+////  LegacyOrderLineTotal.swift
+////  MobileBench
+////
+////  Created by Michael Rutherford on 9/12/20.
+////
 //
-//  LegacyOrderLineTotal.swift
-//  MobileBench
+//import Foundation
+//import MoneyAndExchangeRates
 //
-//  Created by Michael Rutherford on 9/12/20.
-//
-
-import Foundation
-import MoneyAndExchangeRates
-
-public struct LegacyOrderLineTotal {
-    public var qtyOrdered: Int
-    public var qtyShipped: Int
-    public var qtyDiscounted: Int
-    
-    public var totalFrontlinePrice: MoneyWithoutCurrency
-    public var totalDiscount: MoneyWithoutCurrency
-    public var totalDeposit: MoneyWithoutCurrency
-    
-    public init(_ line: LegacyOrderLine) {
-        if line.isDiscountOnly {
-            qtyOrdered = 0
-            qtyShipped = 0
-            qtyDiscounted = line.qtyDiscounted
-            totalDiscount = qtyDiscounted * line.unitDisc
-            totalFrontlinePrice = .zero
-            totalDeposit = .zero
-            return
-        }
-        
-        qtyOrdered = line.qtyOrdered
-        qtyShipped = line.qtyShipped
-        qtyDiscounted = 0
-        
-        qtyDiscounted = qtyShipped
-        totalDiscount = qtyShipped * line.unitDisc
-        
-        totalFrontlinePrice = qtyShipped * line.unitPrice
-        
-        totalDeposit = qtyShipped * line.unitDeposit
-    }
-}
+//public struct LegacyOrderLineTotal {
+//    public var qtyOrdered: Int
+//    public var qtyShipped: Int
+//    public var qtyDiscounted: Int
+//    
+//    public var totalFrontlinePrice: MoneyWithoutCurrency
+//    public var totalDiscount: MoneyWithoutCurrency
+//    public var totalDeposit: MoneyWithoutCurrency
+//    
+//    public init(_ line: LegacyOrderLine) {
+//        if line.isDiscountOnly {
+//            qtyOrdered = 0
+//            qtyShipped = 0
+//            qtyDiscounted = line.qtyDiscounted
+//            totalDiscount = qtyDiscounted * line.unitDisc
+//            totalFrontlinePrice = .zero
+//            totalDeposit = .zero
+//            return
+//        }
+//        
+//        qtyOrdered = line.qtyOrdered
+//        qtyShipped = line.qtyShipped
+//        qtyDiscounted = 0
+//        
+//        qtyDiscounted = qtyShipped
+//        totalDiscount = qtyShipped * line.unitDisc
+//        
+//        totalFrontlinePrice = qtyShipped * line.unitPrice
+//        
+//        totalDeposit = qtyShipped * line.unitDeposit
+//    }
+//}
--- Sources/MobileLegacyOrder/LegacyOrderTotal.swift
+++ Sources/MobileLegacyOrder/LegacyOrderTotal.swift
@@ -1,27 +1,27 @@
+////
+////  MobileOrderTotal.swift
+////  MobileBench
+////
+////  Created by Michael Rutherford on 9/12/20.
+////
 //
-//  MobileOrderTotal.swift
-//  MobileBench
+//import Foundation
+//import MoneyAndExchangeRates
 //
-//  Created by Michael Rutherford on 9/12/20.
+//public struct LegacyOrderTotal {
+//    public var totalFrontlinePrice: MoneyWithoutCurrency = .zero
+//    public var totalDiscount: MoneyWithoutCurrency = .zero
+//    public var totalDeposit: MoneyWithoutCurrency = .zero
 //
-
-import Foundation
-import MoneyAndExchangeRates
-
-public struct LegacyOrderTotal {
-    public var totalFrontlinePrice: MoneyWithoutCurrency = .zero
-    public var totalDiscount: MoneyWithoutCurrency = .zero
-    public var totalDeposit: MoneyWithoutCurrency = .zero
-
-    public var totalDue: MoneyWithoutCurrency {
-        totalFrontlinePrice - totalDiscount + totalDeposit
-    }
-
-    public init(_ lines: [LegacyOrderLineTotal]) {
-        for line in lines {
-            totalFrontlinePrice += line.totalFrontlinePrice
-            totalDiscount += line.totalDiscount
-            totalDeposit += line.totalDeposit
-        }
-    }
-}
+//    public var totalDue: MoneyWithoutCurrency {
+//        totalFrontlinePrice - totalDiscount + totalDeposit
+//    }
+//
+//    public init(_ lines: [LegacyOrderLineTotal]) {
+//        for line in lines {
+//            totalFrontlinePrice += line.totalFrontlinePrice
+//            totalDiscount += line.totalDiscount
+//            totalDeposit += line.totalDeposit
+//        }
+//    }
+//}
